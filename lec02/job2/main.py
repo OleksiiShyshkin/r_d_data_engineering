9a793a81623ce2b555b3ecfc3c07fd0aa6ca84a0
oleksii.shyshkin <alexshyshkin7@gmail.com>
2025-11-04 22:33:12 +0200

    job2: json→avro service (fastavro), Flask on 8082

---
+++ lec02/job2/main.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify
+from .bll.convert_to_avro import save_avro_from_json
+
+app = Flask(__name__)
+
+@app.route("/stg", methods=["POST"])
+def run_job2():
+    payload = request.get_json(force=True)
+    raw_dir = payload.get("raw_dir")
+    stg_dir = payload.get("stg_dir")
+
+    if not raw_dir or not stg_dir:
+        return jsonify({"error": "raw_dir і stg_dir обов'язкові"}), 400
+
+    try:
+        res = save_avro_from_json(raw_dir=raw_dir, stg_dir=stg_dir)
+        return jsonify({"status": "ok", **res}), 201
+    except Exception as e:
+        return jsonify({"status": "error", "message": str(e)}), 500
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8082)
